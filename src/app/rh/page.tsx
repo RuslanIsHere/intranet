c95f00a85d43e90b9300845c3fee13892a518951
RuslanIsHere <gabitovrv@outlook.com>
2025-06-03 12:54:38 +0200

    RH page logic improved with section

--- src/app/rh/page.tsx
+++ src/app/rh/page.tsx
@@ -1,49 +1,6 @@
-'use client'
-
-import { Box, Typography, Divider, Button } from '@mui/material'
-import RHList from '@/components/rh/RHList'
-import RHEditorModal from '@/components/rh/RHEditorModal'
-import { useUserContext } from '@/context/UserContext'
-import { useState } from 'react'
-import { supabase } from '@/utils/supabase/client'
+import { RHSection } from '@/sections/RHSection';
 
 export default function RHPage() {
-    const { profile } = useUserContext()
-    const [open, setOpen] = useState(false)
-
-    const handleSave = async ({ title, content }: { title: string; content: string }) => {
-        const { error } = await supabase.from('rh').insert({
-            title,
-            content,
-            added_by: profile?.id,
-        })
-
-        if (error) {
-            console.error('Erreur ajout RH:', error)
-        } else {
-            location.reload()
-        }
-    }
-
-    return (
-        <Box px={{ xs: 2, sm: 4 }} py={4}>
-            <Typography variant="h4" fontWeight="bold" gutterBottom>
-                Ressources Humaines
-            </Typography>
-
-            <Divider sx={{ mb: 3 }} />
-
-            {profile?.roles?.includes('admin') && (
-                <Box mb={3}>
-                    <Button variant="contained" onClick={() => setOpen(true)}>
-                        Ajouter un article
-                    </Button>
-                </Box>
-            )}
-
-            <RHList />
-
-            <RHEditorModal open={open} onClose={() => setOpen(false)} onSave={handleSave} />
-        </Box>
-    )
+    return<RHSection />;
 }
+
